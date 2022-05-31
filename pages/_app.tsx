449bb56312c089234a9bef4df05188f2ef493ed7
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-31 16:44:23 +0200

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import "@styles/global.css";
+import { RecoilRoot } from "recoil";
+import { appWithTranslation } from "@i18n";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
