0aaaa8bda2e80324341deabdb36bd28cb7110954
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-06-29 12:30:22 +0200

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,36 @@ import "tailwindcss/tailwind.css";
 import "@styles/global.css";
 import { RecoilRoot } from "recoil";
 import { appWithTranslation } from "@i18n";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <RecoilRoot>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Alexandre Houmeau</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Component {...pageProps} />
     </RecoilRoot>
   );
