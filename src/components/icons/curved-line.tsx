1fe4bacd9c6e6ad7181bceafb1330aa90802bc32
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-07-01 23:34:27 +0200

    added Icons

--- src/components/icons/curved-line.tsx
+++ src/components/icons/curved-line.tsx
@@ -3,16 +3,17 @@ import * as React from "react";
 function CurvedLine(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="299"
-      height="388"
-      viewBox="0 0 299 388"
+      width="514"
+      height="455"
+      viewBox="0 0 514 455"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        d="M7.22121 329.168C7.22121 329.168 -35.2488 63.4177 101.741 314.098C238.731 564.778 108.591 88.0677 108.591 88.0677C108.591 88.0677 42.8412 -172.202 297.631 207.248"
-        stroke-miterlimit="10"
+        d="M1 1C50.2027 56.6635 181.632 153.66 313.729 96.3365C478.85 24.6823 572.418 284.58 470.844 326.48C369.269 368.379 126.592 212.926 79.5575 326.48C41.9299 417.323 81.5589 449.344 106.077 454"
+        stroke="#5585DA"
+        strokeDasharray="8 8"
       />
     </svg>
   );
