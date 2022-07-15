e7c6efcd6afaa22c28c5c6d94b0b2bb9cebb39fe
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-07-15 09:17:34 +0200

    Updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
-import { Container, Header } from "@components";
-import { AboutMe, Experience, Github, Main, Skills, SkillsMobile } from "@layouts";
+import { Container, CookieModal, Header } from "@components";
+import {
+  AboutMe,
+  Contact,
+  ContactMobile,
+  Experience,
+  Github,
+  Main,
+  Skills,
+  SkillsMobile,
+} from "@layouts";
 import { Parallax } from "@react-spring/parallax";
 import { isMobile } from "react-device-detect";
+import { AnimatePresence } from "framer-motion";
 
 interface PropType {
   contributions: {
@@ -27,16 +37,34 @@ interface PropType {
 
 const Home: React.FC<PropType> = ({ contributions }) => {
   let parallax;
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const acceptedCookie = localStorage.getItem("cookie");
+      if (!acceptedCookie) {
+        setShow(true);
+      }
+    }, 3000);
+  }, []);
+
+  const acceptCookie = () => {
+    setShow(false);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("cookie", "true");
+    }
+  };
   return (
     <Container>
       <div className="sticky z-50 w-full top-0">
         <Header />
       </div>
       <Parallax
-        pages={5}
+        pages={6}
         innerStyle={{ overflow: "initial" }}
-        style={{ top: "0", left: "0", height: "100vh", overflow: "scroll"}}
+        style={{ top: "0", left: "0", height: "100vh", overflow: "scroll" }}
         ref={(ref) => (parallax = ref)}
+        className="scrollingContainer"
       >
         {/* <ParallaxLayer> */}
         <Main />
@@ -50,16 +78,18 @@ const Home: React.FC<PropType> = ({ contributions }) => {
         {/* </ParallaxLayer> */}
 
         {/* <ParallaxLayer factor={4} offset={3}> */}
-        {isMobile ? (
-          <SkillsMobile />
-        ) : (
-          <Skills />
-        )}
+        {isMobile ? <SkillsMobile /> : <Skills />}
         {/* </ParallaxLayer> */}
         {/* <ParallaxLayer offset={6}> */}
         <Github contributions={contributions} />
+
+        {isMobile ? <ContactMobile /> : <Contact />}
+
         {/* </ParallaxLayer> */}
       </Parallax>
+      <AnimatePresence>
+        {show && <CookieModal closeModal={acceptCookie} />}
+      </AnimatePresence>
     </Container>
   );
 };
