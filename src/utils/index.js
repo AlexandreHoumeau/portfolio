e01a80912f0d3d76e4f324c55c6dd53c7b1b85af
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-07-19 17:08:58 +0200

    fix fix fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,15 @@
 export const pageview = (url) => {
   const { NEXT_PUBLIC_GA_API_KEY } = process.env;
-  window.gtag('config', NEXT_PUBLIC_GA_API_KEY, {
-    page_path: url,
-  })
-}
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("config", NEXT_PUBLIC_GA_API_KEY, {
+      page_path: url,
+    });
+  }
+};
 
 // log specific events happening.
-export const event = ({ action, params  }) => {
-  window.gtag('event', action, params)
-}
+export const event = ({ action, params }) => {
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("event", action, params);
+  }
+};
