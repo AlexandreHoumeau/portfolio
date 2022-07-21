21ebb1c8f62c4d0b58a3623f7534c02683deb997
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-07-21 16:02:40 +0200

    added pdf attachements

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,15 +1,32 @@
 const sgMail = require("@sendgrid/mail");
+const fs = require("fs");
+
+function base64_encode(file: any) {
+  var bitmap = fs.readFileSync(file);
+  return new Buffer(bitmap).toString("base64");
+}
 
 export default function handler(req: any, res: any) {
   const { email, name, role, quality, comment } = req.body;
+  let data_base64 = base64_encode('public/pdf/my_resume.pdf');
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  
+
   if (req.method === "POST" && email) {
     const msg = [
       {
         to: email, // Change to your recipient
         from: "contact@alexandre-houmeau.com", // Change to your verified sender
         templateId: "d-b2c8ac2fb3174c72b0f5a7d1101783c2",
+        attachments: [
+          {
+            filename: `my_resume`,
+            content: data_base64,
+            type: 'application/pdf',
+            disposition: 'attachment'
+          }
+         ]
+        
       },
       {
         to: "alexandre.houmeau@gmail.com", // Change to your recipient
