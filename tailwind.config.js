d2683be62faeef1900b9474586b9cc52558fa866
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-07-25 13:48:18 +0200

    updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       fontFamily: {
         Constructio: ["ConstructioGrotesk-Regular"],
         Courier: ["courier-std"],
-        CourierBold: ["courier-bold"],
+        CourierBold: ["Courier-Bold"],
         Abril: ["Abril Fatface"],
       },
     },
